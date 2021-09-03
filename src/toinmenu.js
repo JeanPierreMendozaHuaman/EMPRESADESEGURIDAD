9a5459986e2c2c5b40662bd1b4825ebad1455df3
terna2021 <81658115+terna2021@users.noreply.github.com>
2021-09-03 01:20:46 -0500

    Update toinmenu.js

--- src/toinmenu.js
+++ src/toinmenu.js
@@ -1,11 +1,5 @@
 const toinmenu = (prefix, pushname) => {
-    return `◪ *Comandos de Shan*
-    │
-    ├─ ❏ ${prefix}setprefix
-    ├─ ❏ ${prefix}block
-    ├─ ❏ ${prefix}bc
-    ├─ ❏ ${prefix}bcgc
-    └─ ❏ ${prefix}clearall`
+    return `start`
 
 }
 

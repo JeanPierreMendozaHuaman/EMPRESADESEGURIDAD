f8a6572bc031a53cdcd6b5c325b4c58ffb06fb94
terna2021 <81658115+terna2021@users.noreply.github.com>
2021-09-03 01:38:33 -0500

    Update banmenu.js

--- src/banmenu.js
+++ src/banmenu.js
@@ -1,29 +1,5 @@
 const banmenu = (prefix, pushname) => {
-    return `*Banear El Uso Del Bot*
-    
- _El bot necesita de admin_
-
-Este comando impide que el baneado use el bot
-
-══════════════
-
-*PARA BANEAR*
-
-*ban + a la persona que deseas banear
-
-Ejemplo: *ban @xxxxxxx
-
-══════════════
-
-*PARA DESBANEAR*
-
-*desban + a la persona que deseas desbanear
-
-Ejemplo: *desban @xxxxxxx
-
-══════════════
-
-ву ѕнαη∂υу`
+    return `ronaldoboot`
 
 }
 

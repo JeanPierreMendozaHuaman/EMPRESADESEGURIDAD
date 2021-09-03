6216faf83ffd5a8d1f690b6cce44d303b510ce91
Ronaldoboot <89986493+Ronaldoboot@users.noreply.github.com>
2021-09-02 19:07:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -44,22 +44,7 @@ const _leveling = JSON.parse(fs.readFileSync('./database/json/leveling.json'))
 const _level = JSON.parse(fs.readFileSync('./database/json/level.json'))
 /******FIN DE ENTRADA JSON******/
 
-/******INICIO DE LA ENTRADA DEL MENÚ******/
-const { help } = require('./src/help')
-const { logomaker } = require('./database/menu/logomaker')
-const { toinmenu } = require('./src/toinmenu')
-const { menuadmin } = require('./src/menuadmin')
-const { nsfwmenu } = require('./src/nsfwmenu')
-const { desmenu } = require('./src/desmenu')
-const { version } = require('./src/version')
-const { juegos } = require('./src/juegos')
-const { shantera } = require('./src/shantera')
-const { antimenu } = require('./src/antimenu')
-const { welmenu } = require('./src/welmenu')
-const { banmenu } = require('./src/banmenu')
-const { otak } = require('./src/otak')
-const { levelmenu } = require('./src/levelmenu')
-/******FIN DE ENTRADA DEL MENÚ******/
+
 
 /******CARGA DE ENTRADA VCARD******/
 const vcard = 'BEGIN:VCARD\n' // Tarjeta de contacto
